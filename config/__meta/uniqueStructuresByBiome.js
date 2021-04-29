62051c961d5c5b9619771cc8dbed90b6474ab64c
danitheturtle <danitheturtle@protonmail.com>
2021-04-28 20:36:10 -0400

    added old sequoia forest and reorganized recurrent complex structures

---
+++ config/__meta/uniqueStructuresByBiome.js
@@ -0,0 +1,22 @@
+let customBiomes = {
+  "Old Sequoia Forest": {
+    "spawnRedwoods.rcbp": ["Redwood", "RedwoodFirGiant", "RedwoodFirGiant2"]
+    "$MUSHROOM": [ //All mushrooms spawn as trees with weight 0.5
+      "RedMushroom", "RedMushroom1", "RedMushroom2", "RedMushroom3", "BrownMushroom", "BrownMushroom"
+    ]
+  }
+}
+
+let treeTypes = {
+  "spawnConiferTree.rcbp": ["DouglasFirGiant", "AlascaCedar", "FirTreeBig"],
+  "spawnBirchTrees.rcbp": [
+    "AlderBirch", "AlderLargeBirch", "AmazonBirch", "BirchTree", "AspenBirch", "BirchTreeBig",
+    "CinchonaBirch", "FullSilverBirch", "HulboonBirch", "HuraBirch", "JackalberryBirch",
+    "JackalberryLargeBirch", "LedgerBirch", "MachoBirch", "PaperBirch", "PaperLargeBirch",
+    "ParicaBirch", "QuakingAspen", "SandboxBirch", "SausageBirch", "SilverBirch",
+    "SmallSausageBirch", "TambotiBirch", "YagrumoBirch"
+  ],
+  "spawnForestTrees.rcbp": [
+    "GenericTree", "GenericTree2", "GenericTree5", "GiganticTree", "GiganticTree1", "GenericTreeHuge"
+  ]
+}
